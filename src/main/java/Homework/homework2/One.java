da2be986e2103396acbf889f5a3f678adc25a676
linuxguy467 <dpplrdr435@gmail.com>
2017-01-20 12:11:51 -0500

    Restructured Repository

---
+++ src/main/java/Homework/homework2/One.java
@@ -0,0 +1,7 @@
+package Homework.homework2;
+
+/**
+ * Created by mchemingway0031 on 1/20/2017.
+ */
+public class One {
+}
---
+++ src/test/java/Test.java
@@ -0,0 +1,5 @@
+/**
+ * Created by mchemingway0031 on 1/20/2017.
+ */
+public class Test {
+}
