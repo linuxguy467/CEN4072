a9379862d18f2fe2b2c0c320d9e5fb00979aa1aa
linuxguy467 <dpplrdr435@gmail.com>
2017-03-23 20:46:38 -0400

    Added homework 4 directories (main and test) and added EasyMock dependency.

--- src/test/java/Homework/homework4/TestDefaultController.java
+++ src/test/java/Homework/homework4/TestDefaultController.java
@@ -1,25 +1,41 @@
 package Homework.homework4;
 
+import com.googlecode.easymockrule.EasyMockRule;
+import org.easymock.Mock;
+import org.easymock.TestSubject;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 import static org.easymock.EasyMock.*;
 import static org.junit.Assert.assertEquals;
 
 /**
- * CRN: 10465
- * Course ID: CEN 4072
+ * CRN: 10421
+ * ID: CEN 4072
  * @author 0031
  * Assignment #4
  */
 public class TestDefaultController {
 	/**
-	 * Global instance variables
+	 * Global instance variables (Mocks, Rule and the test subject
+	 * DefaultController Object).
 	 */
+
+	@Rule
+	public EasyMockRule easyMockRule = new EasyMockRule(this);
+
+	@TestSubject
 	private DefaultController controller;
+
+	@Mock
 	private Request request1, request2;
+
+	@Mock
 	private RequestHandler handler1, handler2;
+
+	@Mock
 	private Response response1, response2;
 
 	/**
@@ -32,29 +48,35 @@ public class TestDefaultController {
 	 * will perform and makes them ready for use respectively.
 	 * Also it adds handlers for the RequestHandlers.
 	 *
-	 * @throws Exception
+	 * @throws Exception -- Assertion Exception will be thrown if
+	 * incorrect operation is performed
 	 */
 	@Before 
 	public void setUp() throws Exception{
-		String name = "request1";
-		controller = new DefaultController();	//the default controller object to be used
-		request1 = createNiceMock(Request.class);	//a mock of a request
-		handler1 = createNiceMock(RequestHandler.class);	//mock of requestHandler
-		response1 = createNiceMock(Response.class);		//mock of response2
-		expect(request1.getName()).andReturn(name);	//adding a behavior or request mock
-		replay(request1);	//makes request mock ready for use
-		expect(handler1.process(request1)).andReturn(response1);	//adding a behavior to a requestHandelr mock
-		replay(handler1);	//makes mock ready for use
-		controller.addHandler(request1, handler1); //adds a handler to controller
-		request2 = createNiceMock(Request.class);	//another mock of a request
-	 	expect(request2.getName()).andReturn("request2");	//adds behavior to request mock
-	 	replay(request2); // makes mock ready for use
-	 	handler2 = createNiceMock(RequestHandler.class);	//another RequestHandler mock
-	 	response2 = createNiceMock(Response.class);	//another Response mock
-	 	expect(handler2.process(request2)).andReturn(response2);	//adding behavior to RequestHanderl mock
-	 	replay(handler2); 	//makes mock ready for use
+		controller = new DefaultController();
+		request1 = createNiceMock(Request.class);
+		handler1 = createNiceMock(RequestHandler.class);
+		response1 = createNiceMock(Response.class);
+		expect(request1.getName()).andReturn("request1");
+		replay(request1);
+		expect(handler1.process(request1)).andReturn(response1);
+		replay(handler1);
+		controller.addHandler(request1, handler1);
+		request2 = createNiceMock(Request.class);
+	 	expect(request2.getName()).andReturn("request2");
+	 	replay(request2);
+	 	handler2 = createNiceMock(RequestHandler.class);
+	 	response2 = createNiceMock(Response.class);
+	 	expect(handler2.process(request2)).andReturn(response2);
+	 	replay(handler2);
 	}
-	
+
+	/**
+	 * Tears Down the objects when finished
+	 *
+	 * @throws Exception -- Assertion Exception will be thrown if
+	 * incorrect operation is performed
+	 */
 	@After
 	public void tearDown() throws Exception {
 		controller = null;
@@ -65,36 +87,50 @@ public class TestDefaultController {
 		handler2 = null;
 		response2 = null;
 	}
-	
-	//This method checks to see if handler1 is the handler that is responding to
-	// the Request of request1
+
+	/**
+	 * Checks to see if handler1 is the handler that is responding to
+	 * the Request of request1
+	 *
+	 * @throws Exception -- Assertion Exception will be thrown if
+	 * incorrect operation is performed
+	 */
  	@Test
  	public void testGetHandler() throws Exception{
  		assertEquals(handler1, controller.getHandler(request1));
  	}
- 	
- 	//This method checks that Response response1 is what is processed by the controller
- 	// from the Request of request1
+
+	/**
+	 * Checks that Response response1 is what is processed by the controller
+	 * from the Request of request1
+	 *
+	 * @throws Exception -- Assertion Exception will be thrown if
+	 * incorrect operation is performed
+	 */
  	@Test
  	public void testProcessRequest() throws Exception{	
  		assertEquals(response1, controller.processRequest(request1));
  	}
- 	
- 	//This method checks to see if an Exception is thrown when the controller
- 	//tries to find a handler for a request that has not been added for it. 
- 	//request2.addHandler has not been implemented for any requestHandler. Therefore it
- 	//throws the RuntimeException
+
+	/**
+	 * Checks to see if an Exception is thrown when the controller
+	 * attempts to find a handler for a request that has not been added for it.
+	 * request2.addHandler has not been implemented for any requestHandler. Therefore it
+	 * throws the RuntimeException
+	 */
  	@Test(expected=RuntimeException.class)
- 	public void testGetHandlerUnDefined(){
+ 	public void testGetHandlerRuntime(){
  		controller.getHandler(request2);
  	}
- 	
- 	//This method checks to see if an exception is thrown when the controller tries to 
- 	//add another handler on to request that has all ready been added to the requestHanler
- 	//map.  request1 is already mapped to RequestHandler handler1.  Therefore when it tries to have
- 	//handler2 mapped to it, it throws a RunTimeException.
+
+	/**
+	 * Checks to see if an exception is thrown when the controller attempts to
+	 * add another handler on to request1 that has all ready been added to the requestHandler
+	 * map. request1 is already mapped to RequestHandler handler1. Therefore when it tries to have
+	 * handler2 mapped to it, it throws a RunTimeException.
+	 */
  	@Test(expected=RuntimeException.class)
- 	public void testAddRequestDuplicateName(){
+ 	public void testAddRequestRuntime(){
  		controller.addHandler(request1, handler2);
  	}
 
